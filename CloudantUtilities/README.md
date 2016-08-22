9ea9a0b83317a5991c97fb7fd0f385a7dc36ddde
Ryan Berger <bergerr@us.ibm.com>
2016-08-22 13:55:48 -0400

    Added the cloudant adapter

---
+++ CloudantUtilities/README.md
@@ -0,0 +1,129 @@
+# MobileFirst Utilities Adapter
+
+*All endpoints below should be prefixed with the url `http://{MobileFirst server}/mfp/api/adapters/CloudantUtilities`*
+
+## Document Structure
+
+#### Work Orders
+
+```json
+{
+  "_id": "303012250",
+  "_rev": "10-3e60aeee4f58b3da87ee77c889c86fb4",
+  "addedBy": "Cosmo Kramer",
+  "assignedTo": "George Costanza",
+  "location": "Durham, NC",
+  "orderNumber": 2,
+  "created": "Aug 11, 2016 1:47:59 PM",
+  "inspectionFinished": true,
+  "details": 
+  {
+    "model": "Dishwasher 1",
+    "serial": "123ABC",
+    "manufacturer": "Sears",
+    "inspectedBy": "George Costanza",
+    "failReason": "Some reason here.",
+    "fileName": "Sears-dishwasher1",
+    "notes": "Notes go here.",
+    "manufactureYear": 2016,
+    "lastInspected": "Oct 20, 2013 8:00:00 PM",
+    "date": "Aug 11, 2016 4:05:09 PM",
+    "inspectionPass": false
+  }
+}
+```
+- Required fields
+	- `addedBy`
+	- `orderNumber` 
+	- `inspectionFinished` - boolean "true" or "false"
+- Conditional fields
+	- `details` - only if `inspectionFinished` is "true"
+	- `inspectedBy` - only if `inspectionFinished` is "true"
+	- `inspectionPass` 
+		- only if `inspectionFinished` is "true"
+		- boolean "true" or "false"
+	- `failReason` - only if `inspectionPass` is "true"
+- Optional fields
+	- `asignedTo`
+	- `location`
+	- `model`
+	- `serial`
+	- `manufacturer`
+	- `manufactureYear`
+	- `lastInspected`
+	- `fileName`
+	- `notes`
+- Auto-generated fields (DO NOT USE)
+	- `_id` 
+		- random number from 0 - 1,000,000,000 
+		- used to search for users
+	- `_rev`
+	- `created`
+	- `date`
+	- `valid` - always "true" 
+
+## Endpoints
+
+This is the json structure for POSTing and PUTing users: 
+
+```json
+{
+  "orderNumber":2,
+  "addedBy":"Cosmo Kramer",
+  "assignedTo":"George Costanza",
+  "location":"Durham, NC",
+  "inspectionFinished": true,
+  "details":
+  {
+    "model":"Dishwasher 1",
+    "serial":"123ABC",
+    "manufacturer":"Sears",
+    "manufactureYear":2016,
+    "lastInspected":"2013-10-21",
+    "inspectedBy":"George Costanza",
+    "inspectionPass": false,
+    "failReason":"Some reason here.",
+    "fileName":"Sears-dishwasher1",
+    "notes":"Notes go here."
+  }
+}
+```
+
+### POST `/orders`
+- success: 201 Created
+- failure: 400 Bad Request
+	- a required field is missing
+- failure: 500 Internal Server Error
+	- some element is the wrong type (ex- `details` is a string instead of a json object)
+	- possible conflict, a document with that `_id` may already exist
+
+### GET `/orders/{id}`
+*replace {id} with `_id` from an order document*
+
+- success: 200 OK
+- failure: 404 Not Found
+	- `{id}` may be wrong or the document no longer exists
+
+### PUT `/orders/{id}`
+*replace {id} with `_id` from an order document*
+
+- success: 202 Accepted
+- failure: 400 Bad Request
+	- a required field is missing
+- failure: 500 Internal Server Error
+	- some element is the wrong type (ex- `details` is a string instead of a json object)
+
+### DELETE `/orders/{id}`
+*replace {id} with `_id` from an order document*
+
+- success: 200 OK
+- failure: 404 Not Found
+	- `{id}` may be wrong or the document no longer exists
+
+	
+### GET `/users/{id}`
+*replace {id} with `assignedTo` from an order document*
+
+- success: 200 OK
+- failure: 404 Not Found
+	- `{id}` may be wrong or the document no longer exists
---
+++ CloudantUtilities/src/main/java/com/sample/InspectionDetails.java
@@ -0,0 +1,149 @@
+/**
+* Copyright 2015 IBM Corp.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.sample;
+
+import java.util.Date;
+
+public class InspectionDetails {
+    private String model, serial, manufacturer, inspectedBy, failReason, fileName, notes;
+    private Integer orderNumber, manufactureYear;
+    private Date lastInspected, date;
+    private boolean inspectionPass;
+
+    /**
+     * Strings
+     */
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getInspectedBy() {
+        return inspectedBy;
+    }
+    public void setInspectedBy(String inspectedBy) {
+        this.inspectedBy = inspectedBy;
+    }
+
+    public String getFailReason() {
+        return failReason;
+    }
+    public void setFailReason(String failReason) {
+        this.failReason = failReason;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    /**
+     * Integers
+     */
+    // public Integer getOrderNumber() {
+    //     return orderNumber;
+    // }
+    // public void setOrderNumber(Integer orderNumber) {
+    //     this.orderNumber = orderNumber;
+    // }
+
+    public Integer getManufactureYear() {
+        return manufactureYear;
+    }
+    public void setManufactureYear(Integer manufactureYear) {
+        this.manufactureYear = manufactureYear;
+    }
+
+    /**
+     * Dates
+     */
+    public Date getLastInspected() {
+        return lastInspected;
+    }
+    public void setLastInspected(Date lastInspected) {
+        this.lastInspected = lastInspected;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * Booleans
+     */
+    public boolean getInspectionPass() {
+        return inspectionPass;
+    }
+    public void setInspectionPass(boolean inspectionPass) {
+        this.inspectionPass = inspectionPass;
+    }
+
+    /**
+     * Methods
+     */
+    public boolean isValid() {
+		if (nonNullAndEmpty(inspectedBy)) {
+            if ((!inspectionPass && nonNullAndEmpty(failReason)) || (inspectionPass && !nonNullAndEmpty(failReason))) {
+			    return true;
+            }
+            else {
+                return false;
+            }
+		}
+		else {
+			return false;
+        }
+	}
+
+    public boolean nonNullAndEmpty(String element) {
+        if (element!=null && !element.isEmpty()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
