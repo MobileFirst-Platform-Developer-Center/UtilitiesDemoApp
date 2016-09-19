0bb9b7d185443902bece56a2b5ac82a41f0c68bc
Eric N Garcia <engarcia@us.ibm.com>
2016-09-19 16:19:40 -0400

    Started implementing authentication.

--- utilities-demo-app/www/js/controllers/login.js
+++ utilities-demo-app/www/js/controllers/login.js
@@ -1,13 +1,44 @@
-app.controller('LoginCtrl', function($scope, $state, $ionicHistory, WorkItems) {
-	$ionicHistory.clearHistory();
-	$scope.login = function () {
+// app.controller('LoginCtrl', function($scope, $state, $ionicHistory, WorkItems) {
+// 	$ionicHistory.clearHistory();
+// 	$scope.login = function () {
 		
-		// TODO: Setup login credentials
+// 		// TODO: Setup login credentials
 
 
 
 
 
-		$state.go('workItems');
-	}
+// 		$state.go('workItems');
+// 	}
+// });
+
+app.controller('LoginCtrl', function ($scope, $state, Auth) {
+  var authInProgress = false;
+
+  // TODO: only for testing. Remove before release
+  $scope.username = '1234';
+  $scope.password = '1234';
+
+  $scope.login = function () {
+    if (!authInProgress) {
+      Auth.login($scope, this.username.toLowerCase(), this.password.toLowerCase());
+      authInProgress = true;
+    }
+  };
+  $scope.doDemoLogin = function () {
+    if (!authInProgress) {
+      Auth.login($scope, "demo", "demo");
+      authInProgress = true;
+    }
+  };
+  $scope.$on('login-success', function () {
+    authInProgress = false;
+    $state.go('app.search');
+  });
+
+  $scope.$on('login-error', function (event, error) {
+    alert(error.message);
+
+    authInProgress = false;
+  });
 });
