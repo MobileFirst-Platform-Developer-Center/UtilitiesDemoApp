ee1908472b9f10cce98a82d994af5a116b8cdc1d
Eric N Garcia <engarcia@us.ibm.com>
2016-10-05 16:09:38 -0400

    Updated login to follow model.  It now correctly detects credentials.

--- utilities-demo-app/www/js/controllers/login.js
+++ utilities-demo-app/www/js/controllers/login.js
@@ -1,27 +1,32 @@
+'use strict';
+
 app.controller('LoginCtrl', function ($scope, $state, Auth, $ionicHistory, WorkItems) {
 	$ionicHistory.clearHistory();
 	var authInProgress = false;
-
-	// TODO: only for testing. Remove before release
-	// $scope.username = '1234';
-	// $scope.password = '1234';
+	$scope.credentials = {
+		username: "1234",
+		password: "1234",
+		rememberMe: false
+	};
 
 	$scope.login = function () {
 		console.log("login function entered");
 
 		// Validate input
-		if (!$scope.username || !$scope.password) {
+		if (!$scope.credentials.username || !$scope.credentials.password) {
 			console.log('No username or password provided');
 		} else {
-			console.log('username and password have been provided');
-			if ($scope.username == $scope.password) {
+			console.log('username and password have been provided, remember me is: ' + $scope.credentials.rememberMe);
+			if ($scope.credentials.username == $scope.credentials.password) {
+				console.log('username and password match, proceeding to login');
 				// Proceed to login
 				if (!authInProgress) {
-					Auth.login($scope, $scope.username.toLowerCase(), $scope.password.toLowerCase());
+					Auth.login($scope, $scope.credentials.username.toLowerCase(), $scope.credentials.password.toLowerCase());
 					authInProgress = true;
 				}
 			} else {
 				// Show error username / password does not match.
+				console.log('username and password do not match.');
 			};
 		};
 	};
--- utilities-demo-app/www/js/services/auth.js
+++ utilities-demo-app/www/js/services/auth.js
@@ -1,3 +1,5 @@
+'use strict';
+
 app.factory('Auth', function ($rootScope) {
 
 	var securityCheckName = 'UserLogin',
